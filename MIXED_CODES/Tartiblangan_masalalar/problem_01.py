644cd08cd4a46ed627e1081dfb3bc8077ff932db
JamshidbekShodibekov <jamshidbekdev04@gmail.com>
2024-02-14 03:54:00 +0500

    Create problem_01.py

---
+++ MIXED_CODES/Tartiblangan_masalalar/problem_01.py
@@ -0,0 +1,21 @@
+# n
+# soni
+# berilgan.
+#
+# Quyidagi
+# yig
+# 'indini hisoblovchi dastur yozing.
+#
+# (1 + 1 / 2 + 1 / 3 + 1 / 4..... 1 /n).
+#
+# Namuna(Taxmini
+# son)
+#
+# Berilgan: a = 10
+#
+# Natija: 2.92......
+sum=0
+n=int(input())
+for i in range(n):
+    sum+=1/(i+1)
+print(sum)
