5009d92ae38d546099b7dfcfb5c7c7929e55438d
JamshidbekShodibekov <jamshidbekdev04@gmail.com>
2024-02-14 02:34:43 +0500

    Create code_02.py
    code_02

---
+++ PYTHON_DARSLARI_KURS/Lesson_01/code_02.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 10 01:16:49 2024
+
+@author: hp
+"""
+#not variable  /*-+  ; 5465h"jb'hj,uyyuv"/*-+"
+#variable  gyuuvg_656gfj,fth_jhj,jhjb_6516,htf65856
+a='jfj'
+print(type(a))
+a='jbkgvj'
+b='2'
+print(type(a+b))
+c=5
+#print(int(a)+int(b),"\n",type(c))
+print(a.__sizeof__())
+print(a.__doc__)
+#int,float,str
+print("➡,🔙,🔛,⭐")
+a="➡,🔙,🔛,⭐"
+print(a)
+
