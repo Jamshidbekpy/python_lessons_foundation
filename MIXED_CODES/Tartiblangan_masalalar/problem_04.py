6ed67c0b12f51f6a1bb352aea607672de2f8023c
JamshidbekShodibekov <jamshidbekdev04@gmail.com>
2024-02-14 04:08:22 +0500

    Create problem_04.py

---
+++ MIXED_CODES/Tartiblangan_masalalar/problem_04.py
@@ -0,0 +1,17 @@
+# Misol Sharti
+# n soni berilgan.
+#
+# Geometrik shaklni ekranga chiqaring.
+#
+# Namuna
+#
+# Masalan: n = 5
+#
+#    *********
+#     *******
+#      *****
+#       ***
+#        *
+n=5
+for i in range(n):
+    print(i*" ",(2*(n-i)-1)*"*")
