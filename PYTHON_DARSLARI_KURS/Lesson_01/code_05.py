481c7c8a7db2e6f60f1808cb3fa13ebbac2a994a
JamshidbekShodibekov <jamshidbekdev04@gmail.com>
2024-02-14 03:24:24 +0500

    Create code_05.py

---
+++ PYTHON_DARSLARI_KURS/Lesson_01/code_05.py
@@ -0,0 +1,17 @@
+a='Jamshidbek'
+b='Alimurod'
+c=20
+d=20
+#formatlash
+print('{} {} yoshda,va mening do\'stim.Mening ismim {} va men {} yoshdaman'.format(b,c,a,d))
+print(f'{b} {c} yoshda,va mening do\'stim.Mening ismim {a} va men {d} yoshdaman')
+
+#o'zgaruvchiga qiymat berish
+a,b,c=1,2,'Salom'
+print(a,b,c)
+a=1,2,'Salom'
+print(a)
+#Amallar
+print(3&4)
+print(3^2)
+
