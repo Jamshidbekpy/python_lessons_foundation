34a4f305263b847191f663d4bc68b58fe5767e67
JamshidbekShodibekov <jamshidbekdev04@gmail.com>
2024-02-14 02:26:57 +0500

    code_01 finished

---
+++ PYTHON_DARSLARI_KURS/Lesson_01/code_01.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 10 00:36:11 2024
+
+@author: Jamshidbekdev
+"""
+print("hello world!")
+print('hello world!')
+print("o'")
+print('o\'')
+print("a\tb")
+print("j\nk")
+print("""
+Musaffo osmonda porlaydi quyosh
+Va ko'kda bulutlar suzadi erkin""")
+print(2/8)
+print(2//8)
+print()
+print(2*8)
+print(2**8,end=' ')
+print(2**(1/8))
+print(9%5)
+print("jgbbbi",6+5,end="\t")
+print("Salom")
+
+
