00374e4553e6a6ffce607533c4aeec81119deeb7
JamshidbekShodibekov <jamshidbekdev04@gmail.com>
2024-02-14 02:40:21 +0500

    Create code_03.py

---
+++ PYTHON_DARSLARI_KURS/Lesson_01/code_03.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 10 01:37:52 2024
+
+@author: hp
+"""
+a=" Hello WoRld! "
+print(a.capitalize())
+print(a.title())
+print(a.upper())
+print(a.lower())
+print(a.lstrip())
+print(a.rstrip())
+print(a.strip())
+print(a.replace('Hello',"a"))
+print(len(a))
+a=448_686_568_746_586_764_546
+print(a)
+a,b,c=3,9,5
+print(a,b,c)
+#const e'lon qilish
+PI=3.14
+print(PI)
+print(help())
+
+
+
